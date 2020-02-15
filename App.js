dd53b76beb11cac074a66b988bf2dbf994c2ce92
@maxgfr <maxgolfier@gmail.com>
2020-02-15 20:45:59 +0100

    push

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
