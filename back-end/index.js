963eba9bf227975f9173c89a1e03b72c1e5dcac5
@maxgfr <maxgolfier@gmail.com>
2020-02-22 11:21:47 +0100

    push

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SK);
 const app = express();
 const port = 3000;
